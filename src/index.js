831ab9ca39739409b6d35d558146c23148e8bdd7
Mario <mario.olislaegers@gmail.com>
2019-03-19 14:37:07 +0100

    Service & Controller tests

--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
+require( "dotenv" ).config();
 const express = require( "express" );
+const bodyparser = require( "body-parser" );
+const cors = require( "cors" );
+const mongoose = require( "mongoose" );
+const Prometheus = require( "prom-client" );
+const expressPrometheus = require( "express-prom-bundle" );
+const logger = require( "./config/logger" );
 
 const app = express();
-const port = process.env.PORT || 3000;
-const cors = require( "cors" );
-const bodyParser = require( "body-parser" );
-const mongoose = require( "mongoose" );
+app.disable( "x-powered-by" );
 
-const dbUrl = process.env.MONGOLAB_URI || "mongodb://localhost:27017/taskservice";
+// Server config
+const port = process.env.PORT;
+const host = process.env.HOSTNAME;
+const mongoDB = process.env.MONGO_DB;
+const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect( dbUrl, { useNewUrlParser: true } );
-
+// Middleware
+app.use( bodyparser.json() );
 app.use( cors() );
 
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { extended: false } ) );
+//Routes
+const boardRoutes = require("./routes/BoardRoutes");
+app.use("/board", boardRoutes);
 
-const boardRoutes = require( "./routes/BoardRoutes" );
+// Connect database
+mongoose
+    .connect( mongoURI + mongoDB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false } )
+    .then( () => logger.info( "MongoDB connected" ) )
+    .catch( error => logger.error( error.stack ) );
 
-app.use( "/board", boardRoutes );
-app.listen( port );
+// Register prometheus
+Prometheus.collectDefaultMetrics();
+app.use( expressPrometheus() );
 
-console.log( `Task Management Service started on: ${ port }` );
+// Add prometheus
+app.get( "/metrics", ( req, res ) => {
+    res.set( "Content-Type", Prometheus.register.contentType );
+    res.end( Prometheus.register.metrics() );
+} );
+// Not found
+app.all( "*", ( req, res ) => res.status( 404 ).json( {
+    name: "NotFound",
+    message: "Resource Not Found",
+    status: 404,
+} ) );
 
+// Global error handler
+app.use( ( error, req, res ) => {
+    logger.error( "An unhandled exception occurred", error );
+    return res.status( 500 ).json( {
+        name: "InternalServerError",
+        message: "An error ocurred",
+        status: 500,
+    } );
+} );
+
+// Start service
+app.listen( port, () => {
+    logger.info( `Service: listening on http://${ host }:${ port }` );
+} );
 module.exports = app;
--- test/controllers/BoardController.js
+++ test/controllers/BoardController.js
@@ -1,32 +1,42 @@
-const chai = require( "chai" );
-const chaiHttp = require( "chai-http" );
-const app = require( "../../src/index" );
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../../src/index");
 
-chai.use( chaiHttp );
+chai.use(chaiHttp);
 const should = chai.should();
 
-describe( "/create", () => {
-    it( "should create a board", ( done ) => {
-        const req = { name: "boardName" };
-        chai.request( app )
-            .post( "/board/create" ).send( req )
-            .end( ( err, res ) => {
-                res.should.have.status( 200 );
-                res.body.message.should.be.equal( "Created board!" );
-                res.body.board.name.should.be.equal( "boardName" );
+describe("/create", () => {
+    it("should create a board", (done) => {
+        const req = { projectId: "1", name: "boardName" };
+        chai.request(app)
+            .post("/board/create").send(req)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.message.should.be.equal("Created board!");
+                res.body.board.name.should.be.equal("boardName");
                 done();
-            } );
-    } );
-    it( "should create a board with a default name", ( done ) => {
-        const req = {};
-        chai.request( app )
-            .post( "/board/create" ).send( req )
-            .end( ( err, res ) => {
-                res.should.have.status( 200 );
-                res.body.message.should.be.equal( "Created board!" );
-                should.exist( res.body.board.name );
-                should.exist( res.body.board._id );
+            });
+    });
+    it("should create a board with a default name", (done) => {
+        const req = { projectId: "1" };
+        chai.request(app)
+            .post("/board/create").send(req)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.message.should.be.equal("Created board!");
+                should.exist(res.body.board.name);
+                should.exist(res.body.board._id);
                 done();
-            } );
-    } );
-} );
+            });
+    });
+
+    it("should return status 401 if no projectId is specified", (done) => {
+        const req = { };
+        chai.request(app)
+            .post("/board/create").send(req)
+            .end((err, res) => {
+                res.should.have.status(401);
+                done();
+            });
+    });
+});
