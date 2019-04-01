05a882a724f8905edc82287e736ce1f6115df81b
Liwus <liweihu12@gmail.com>
2019-04-01 15:12:03 +0200

    Change req.body to use req.params in get requests and delete requests

--- test/controllers/TaskController.js
+++ test/controllers/TaskController.js
@@ -15,30 +15,24 @@ describe( "TaskController", () => {
 
     describe( "/create", () => {
         it( "should create a task with the name and description", ( done ) => {
-            const req = { columnId: "5c921b99bf81ef15fc6eb29a", name: "taskName", description: "taskDescription" };
             chai.request( app )
-                .post( "/task/create" ).send( req )
+                .post( "/board/create" ).send( { projectId: "1", name: "boardName" } )
                 .end( ( err, res ) => {
-                    res.should.have.status( 201 );
-                    should.exist( res.body.message );
-                    res.body.message.should.be.equal( "Created the task!" );
-                    res.body.task.name.should.be.equal( "taskName" );
-                    res.body.task.description.should.be.equal( "taskDescription" );
-                    done();
-                } );
-        } );
-
-        it( "should create a task with the name", ( done ) => {
-            const req = { columnId: "5c921b99bf81ef15fc6eb29a", name: "taskName" };
-            chai.request( app )
-                .post( "/task/create" ).send( req )
-                .end( ( err, res ) => {
-                    res.should.have.status( 201 );
-                    should.exist( res.body.message );
-                    res.body.message.should.be.equal( "Created the task!" );
-                    res.body.task.name.should.be.equal( "taskName" );
-                    res.body.task.description.should.be.equal( "" );
-                    done();
+                    chai.request( app )
+                        .post( "/column/create" ).send( { boardId: res.body.board.id, name: "testBoard" } )
+                        .end( ( createErr, createRes ) => {
+                            const req = { columnId: createRes.body.column.id, name: "taskName", description: "taskDescription" };
+                            chai.request( app )
+                                .post( "/task/create" ).send( req )
+                                .end( ( _err, _res ) => {
+                                    _res.should.have.status( 201 );
+                                    should.exist( _res.body.message );
+                                    _res.body.message.should.be.equal( "Created the task!" );
+                                    _res.body.task.name.should.be.equal( "taskName" );
+                                    _res.body.task.description.should.be.equal( "taskDescription" );
+                                    done();
+                                } );
+                        } );
                 } );
         } );
 
@@ -75,30 +69,33 @@ describe( "TaskController", () => {
 
     describe( "/delete", () => {
         it( "should return status 204 if a task was deleted", ( done ) => {
-            const req = { columnId: "5c921b99bf81ef15fc6eb29a", name: "taskName", description: "taskDescription" };
             chai.request( app )
-                .post( "/task/create" ).send( req )
+                .post( "/board/create" ).send( { projectId: "1", name: "boardName" } )
                 .end( ( err, res ) => {
                     chai.request( app )
-                        .delete( `/task/delete${ res.body.task.id }` ).send()
-                        .end( ( deleteErr, deleteRes ) => {
-                            deleteRes.should.have.status( 204 );
-                            done();
+                        .post( "/column/create" ).send( { boardId: res.body.board.id, name: "testBoard" } )
+                        .end( ( createErr, createRes ) => {
+                            const req = { columnId: createRes.body.column.id, name: "taskName", description: "taskDescription" };
+                            chai.request( app )
+                                .post( "/task/create" ).send( req )
+                                .end( ( _err, _res ) => {
+                                    chai.request( app )
+                                        .delete( `/task/delete/${ _res.body.task.id }` ).send()
+                                        .end( ( deleteErr, deleteRes ) => {
+                                            deleteRes.should.have.status( 204 );
+                                            done();
+                                        } );
+                                } );
                         } );
                 } );
         } );
 
-        it( "should return status 400 if the id was not specified", ( done ) => {
-            const req = { columnId: "5c921b99bf81ef15fc6eb29a", name: "taskName", description: "taskDescription" };
+        it( "should return status 500 if the id was not specified", ( done ) => {
             chai.request( app )
-                .post( "/task/create" ).send( req )
-                .end( () => {
-                    chai.request( app )
-                        .delete( "/task/delete/" ).send( {} )
-                        .end( ( deleteErr, deleteRes ) => {
-                            deleteRes.should.have.status( 400 );
-                            done();
-                        } );
+                .delete( "/task/delete/-1" ).send( {} )
+                .end( ( deleteErr, deleteRes ) => {
+                    deleteRes.should.have.status( 500 );
+                    done();
                 } );
         } );
     } );
