b27993d5df8de2035ff3cef816fd441ba5af7aae
Mario <mario.olislaegers@gmail.com>
2019-04-02 10:00:53 +0200

    Fixed routing

--- src/routes/BoardRoutes.js
+++ src/routes/BoardRoutes.js
@@ -3,7 +3,7 @@ const boardController = require( "../controllers/BoardController" );
 
 const router = express.Router();
 
-router.get( "/getAll/:projectId", boardController.getAll );
+router.get( "/getAll/:id", boardController.getAll );
 router.post( "/create", boardController.createBoard );
 router.put( "/update", boardController.updateBoard );
 router.delete( "/delete/:id", boardController.deleteBoard );
