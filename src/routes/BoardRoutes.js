828fcc68c1ccf66f66e0c058a759ae96a2dc1478
Unknown <liweihu12@gmail.com>
2019-04-07 17:05:35 +0200

    Add get board by id route

--- src/routes/BoardRoutes.js
+++ src/routes/BoardRoutes.js
@@ -3,6 +3,7 @@ const boardController = require( "../controllers/BoardController" );
 
 const router = express.Router();
 
+router.get( "/:id", boardController.getBoardById );
 router.get( "/getAll/:id", boardController.getAll );
 router.post( "/create", boardController.createBoard );
 router.put( "/update", boardController.updateBoard );
