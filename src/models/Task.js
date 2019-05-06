d0d41f8e8abb507636b38fcebd6da8874a57ac8d
Liwus <liweihu12@gmail.com>
2019-05-06 11:35:54 +0200

    Remove position

--- src/models/Task.js
+++ src/models/Task.js
@@ -30,9 +30,6 @@ const uuid = require( "uuid" );
  *           type: number
  *           description: The amount of hours a task should take
  *           example: 2
- *         position:
- *           type: number
- *           description: The position the t ask is in
  *         createdAt:
  *            type: string
  *            description: The date a task was created on
@@ -64,10 +61,6 @@ const Task = new mongoose.Schema( {
         type: String,
     } ],
     hours: {
-        type: Number,
-        default: 0,
-    },
-    position: {
         type: Number,
         default: 0,
     },
